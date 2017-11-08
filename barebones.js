ebddf8f68ffba84a18e936ff932942be854d3a0a
bjohn <bjohn454@gmail.com>
2017-11-07 21:17:01 -0500

    add package, start/stop, and static dir

--- barebones.js
+++ barebones.js
@@ -3,15 +3,27 @@
 var express = require('express');
 var app = express();
 
-var staticDir = '/var/www/static';
+var staticDir = './static';
+
+//create static directory to read files ./static
 app.use(express.static(staticDir));
 
 
-app.listen(4000, function () {
-  console.log('Barebones app listening on port 4000!')
+var serverHandle = app.listen(4000, function () {
+	//start up app on 4000, using npm start
+	//kill using endpoint
+	console.log('Barebones app listening on port 4000!');
 })
 
 app.get('/', function (req, res) {
- console.log(res);
-  res.send('Here is a blank homepage.')
+	//listen in homepage
+	console.log('homepage');
+	res.send('Here is a blank homepage.')
+})
+
+app.get('/kill', function (req, res) {
+	// kill server; never do this in production
+	console.log('killing server');
+	res.send('Killing server...')
+	serverHandle.close();
 })
